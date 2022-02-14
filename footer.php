d041cd7302d35420095d7bb407fe17c3ef1c8de5
rickybobbbby <yannicklanglois99@gmail.com>
2022-02-14 13:15:21 -0500

    Premier commit sur la branche lab4

--- footer.php
+++ footer.php
@@ -17,7 +17,7 @@
              <p>Lorem ipsum dolor sit</p>      
          </section>
 
-         <?php wp_nav_menu(array("menu" => "footer")); ?>
+         <?php wp_nav_menu(array("menu" => "footer", "container" => "nav")); ?>
 
          <section class="site__info__nouvelle">
              <p>Lorem ipsum dolor sit amet consectetur Lorem ipsum dolor sit amet consectetur Lorem ipsum dolor sit amet consectetur Lorem ipsum dolor sit amet consectetur Lorem ipsum dolor sit amet consectetur</p>
