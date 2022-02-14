9e87a321d7a4bc06ef8160bb18b77d1c540588b8
rickybobbbby <yannicklanglois99@gmail.com>
2022-02-14 15:01:17 -0500

    commit fin de cours1 semaine 4 (a completer)

--- footer.php
+++ footer.php
@@ -17,7 +17,9 @@
              <p>Lorem ipsum dolor sit</p>      
          </section>
 
-         <?php wp_nav_menu(array("menu" => "footer", "container" => "nav", "container_class" => "site__footer")); ?>
+         
+         <?php
+          wp_nav_menu(array("menu" => "footer", "container" => "nav", "container_class" => "site__footer__menu__nav", "menu_class" => "footer__menu_ul", "link_before"=>$icone)); ?>
 
          <section class="site__info__nouvelle">
              <p>Lorem ipsum dolor sit amet consectetur Lorem ipsum dolor sit amet consectetur Lorem ipsum dolor sit amet consectetur Lorem ipsum dolor sit amet consectetur Lorem ipsum dolor sit amet consectetur</p>
