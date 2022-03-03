912a97aab2b5df670e83690d5810de680d280ba2
git <e1808378@cmaisonneuve.qc.ca>
2022-03-02 20:15:26 -0500

    commit d'arengement du code, a refaire

--- footer.php
+++ footer.php
@@ -1,17 +1,18 @@
 <footer class="site__footer">
-    <h2 class="footer__titre">Le beau footer</h2>
-    <p class="footer__presentation">Un site web fait par une élève du TIM à l'aide de Wordpress.</p>
-    <h3 class="footer__author">Fait par Florence Lemieux Cayer</h3>
-<?php 
+    <h2 class="footer__titre">Navigation du site</h2>
+    <?php 
 
-$icone = '<svg width="20px" height="20px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" color="#f00"><path fill-rule="evenodd" d="M10.293 15.707a1 1 0 010-1.414L14.586 10l-4.293-4.293a1 1 0 111.414-1.414l5 5a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0z" clip-rule="evenodd"></path><path fill-rule="evenodd" d="M4.293 15.707a1 1 0 010-1.414L8.586 10 4.293 5.707a1 1 0 011.414-1.414l5 5a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>';
-wp_nav_menu(array(
-                    "menu"=>"simple",
-                    "container"=>"nav",
-                    "container_class"=>"site__footer__menu",
-                    "menu_class"=>"site__footer__menu__ul",
+    $icone = '<svg width="20px" height="20px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" color="#f00"><path fill-rule="evenodd" d="M10.293 15.707a1 1 0 010-1.414L14.586 10l-4.293-4.293a1 1 0 111.414-1.414l5 5a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0z" clip-rule="evenodd"></path><path fill-rule="evenodd" d="M4.293 15.707a1 1 0 010-1.414L8.586 10 4.293 5.707a1 1 0 011.414-1.414l5 5a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>';
+    wp_nav_menu(array(
+                        "menu"=>"simple",
+                        "container"=>"nav",
+                        "container_class"=>"site__footer__menu",
+                        "menu_class"=>"site__footer__menu__ul",
 
-                    "link_before"=>$icone)); ?>
+                        "link_before"=>$icone)); ?>
+
+    <p class="footer__presentation">Copyright 2022 - College de Maisonneuve.</p>
+    <h3 class="footer__author">Realiser par Yannick Langlois</h3>
 </footer>
 <?php wp_footer(); ?>
 </body>
