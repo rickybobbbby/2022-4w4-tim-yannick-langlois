1892b65d2d597f29d062c171ca45f0b5a9819f04
git <e1808378@cmaisonneuve.qc.ca>
2022-03-19 21:07:55 -0400

    s8 tp1 debut page, modification du systeme couleur

--- page.php
+++ page.php
@@ -1,11 +1,15 @@
 <?php get_header() ?>
 <main class="site__main">
-
-    <h1>---- page.php ------</h1>
-    <?php if (have_posts()): while(have_posts()): the_post(); ?>
-     <?php the_title() ?>
-     <?php the_content() ?>
-     <?php endwhile ?>
-     <?php endif ?>
+    <section class="page__standard">
+        <?php if (have_posts()): while(have_posts()): the_post(); ?>
+        <div class="page__standard__titre">
+            <h1><?php the_title() ?></h1>
+        </div>
+        <div class="page__standard__contenu">
+            <?php the_content() ?>
+        </div>
+        <?php endwhile ?>
+        <?php endif ?>
+    </section>
 </main>
 <?php get_footer() ?>
